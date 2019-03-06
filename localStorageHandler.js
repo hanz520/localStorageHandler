13f28f7f395714c9a3a566dbcc24439097e9252e
hanz520 <569691266@qq.com>
2019-03-06 17:19:03 +0800

    新增永久储存配置

--- localStorageHandler.js
+++ localStorageHandler.js
@@ -1,37 +1,48 @@
 const localStorageHandler = {
-  dataName: 'myData',
-  setItem: function(name, value) {
-    let obj = JSON.parse(window.localStorage.getItem(this.dataName))
+  dataName: 'ctData',
+  getDataName: function(isEternal) {
+    return isEternal === true ? `${this.dataName}_eternal` : this.dataName
+  },
+  setItem: function(name, value, isEternal) {
+    // 根据isEternal来判断是否永久
+    const dataName = this.getDataName(isEternal)
+    let obj = JSON.parse(window.localStorage.getItem(dataName))
     if(obj == null) {
       obj = {}
     }
     obj[name] = value
-    window.localStorage.setItem(this.dataName, JSON.stringify(obj))
+    window.localStorage.setItem(dataName, JSON.stringify(obj))
   },
-  delItem: function(name) {
-    let obj = JSON.parse(window.localStorage.getItem(this.dataName))
+  delItem: function(name, isEternal) {
+    const dataName = this.getDataName(isEternal)
+    let obj = JSON.parse(window.localStorage.getItem(dataName))
     if(obj === null) {
-      console.info('myData')
       return false
     }else{
       if(obj[name] !== undefined) {
         delete obj[name]
-        window.localStorage.setItem(this.dataName, JSON.stringify(obj))
+        window.localStorage.setItem(dataName, JSON.stringify(obj))
       }
     }
   },
-  getItem: function(name) {
-    let obj = JSON.parse(window.localStorage.getItem(this.dataName))
+  getItem: function(name, isEternal) {
+    const dataName = this.getDataName(isEternal)
+    let obj = JSON.parse(window.localStorage.getItem(dataName))
     if(obj !== null && obj[name] !== undefined) {
       return obj[name]
     }else{
       return null
     }
   },
-  die: function() {
-    window.localStorage.removeItem(this.dataName)
+  die: function(isEternal) {
+    const dataName = this.getDataName(isEternal)
+    window.localStorage.removeItem(dataName)
+  },
+  list: function() {
+    console.log('非永久的', JSON.parse(window.localStorage.getItem(this.dataName)))
+    console.log('永久的', JSON.parse(window.localStorage.getItem(`${this.dataName}_eternal`)))
   }
 }
-window.localStorageHandler = localStorageHandler
+window.LSH = localStorageHandler
 
 export default localStorageHandler
